a697196c201ef016e9945e97d0f78afc76f071c3
Hu <jingliang@tum.de>
2019-12-12 14:13:00 +0100

    input mean-std normalization, shuffle the order of training data for each epoch

--- experiments/0_benchMark/bench_mark_test.py
+++ experiments/0_benchMark/bench_mark_test.py
@@ -21,7 +21,7 @@ print("parameter setting...")
 cudaNow = torch.device('cuda:3')
 nbBatch = 256
 nbEpoch = 200
-learnRate = 1e-4
+learnRate = 1e-3
 momentum = 0.9
 datFlag = 2
 
@@ -30,39 +30,35 @@ trainHistoryFile = 'TrainHistory.h5'
 savePath = envPath+'/trained_models/'+modelName
 histSavePath = envPath+'/trained_models/'+modelName+trainHistoryFile
 
-subtract_pixel_mean = True
-
 
 '''
 STEP ONE: data loading
 '''
 print("data loading...")
 # Load the LCZ42 training data.
-_,x_train_2,y_train,_ = load_Semi_Train(envPath,datFlag)
-x_train = x_train_2
-del x_train_2
+_,x_train,y_train,_ = load_Semi_Train(envPath,datFlag)
 
 # Load the LCZ42 testing data.
-_, x_test_2, y_test, _ = load_Semi_Test(envPath, datFlag)
-x_test = x_test_2
-del x_test_2
-
+_, x_test, y_test, _ = load_Semi_Test(envPath, datFlag)
 
 # Input image dimensions.
 input_shape = x_train.shape[1:]
 
 # Normalize data.
 print("data normalization...")
-v_max = np.max([np.max(x_train),np.max(x_test)])
-v_min = np.min([np.min(x_train),np.min(x_test)])
-x_train = (x_train-v_min) / (v_max-v_min)
-x_test = (x_test-v_min) / (v_max-v_min)
+#v_max = np.max([np.max(x_train),np.max(x_test)])
+#v_min = np.min([np.min(x_train),np.min(x_test)])
+#x_train = (x_train-v_min) / (v_max-v_min)
+#x_test = (x_test-v_min) / (v_max-v_min)
+x_train = mean_Std_Normalization(x_train)
+x_test = mean_Std_Normalization(x_test)
+
 
 # If subtract pixel mean is enabled
-if subtract_pixel_mean:
-    x_train_mean = np.mean(x_train, axis=0)
-    x_train -= x_train_mean
-    x_test -= x_train_mean
+#if subtract_pixel_mean:
+#    x_train_mean = np.mean(x_train, axis=0)
+#    x_train -= x_train_mean
+#    x_test -= x_train_mean
 
 # convert numpy to pytorch float tensor
 x_train = torch.from_numpy(x_train).type('torch.FloatTensor')
@@ -93,7 +89,8 @@ STEP THREE: Define a loss function and optimizer
 import torch.optim as optim
 import torch.nn as nn
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(resnet.parameters(), lr=learnRate, momentum=momentum)
+#optimizer = optim.SGD(resnet.parameters(), lr=learnRate, momentum=momentum)
+optimizer = optim.Adam(resnet.parameters(), lr=learnRate)
 
 
 '''
