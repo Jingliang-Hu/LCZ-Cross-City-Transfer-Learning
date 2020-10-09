ac88ae0950a12949eaf89921d2c78bee24795427
Jingliang <jingliang.hu@dlr.de>
2020-10-09 12:45:44 +0000

    a small clean-up of git repository

--- experiments/2_baseline_42/repeatExpers.py
+++
@@ -1,7 +0,0 @@
-import os
-
-repeatTime = 2
-
-for i in range(0,repeatTime):
-    os.system('python bench_mark_exp.py')
-
--- experiments/4_multi_ensemble/repeatExpers.py
+++
@@ -1,7 +0,0 @@
-import os
-
-repeatTime = 4
-
-for i in range(0,repeatTime):
-    os.system('python ensemble_multi.py')
-
